e7e0af1d008348e3e1b1192cfdd41822ba5a5276
keplerorbit <kepler.orbit@gmail.com>
2015-08-05 12:59:00 +0530

    swagger-codegen-maven-plugin - Test Harness Added

---
+++ swagger-codegen-maven-plugin/src/test/java/CodegenMojoTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.WithoutMojo;
+import org.junit.Rule;
+import org.junit.Test;
+import org.othla.maven.plugin.swagger.codegen.CodeGenMojo;
+
+public class CodegenMojoTest
+{
+	@Rule
+	public MojoRule rule = new MojoRule()
+	{
+		@Override
+		protected void before() throws Throwable
+		{
+		}
+
+		@Override
+		protected void after()
+		{
+		}
+	};
+
+	/**
+	 * @throws Exception
+	 *            if any
+	 */
+	@Test
+	public void testSomething() throws Exception
+	{
+		File pom = new File("src/test/resources/pom.xml");
+		assertNotNull(pom);
+		assertTrue(pom.exists());
+
+		CodeGenMojo myMojo = (CodeGenMojo) rule.lookupMojo("codegen", pom);
+		assertNotNull(myMojo);
+		myMojo.execute();
+
+	}
+
+	/** Do not need the MojoRule. */
+	@WithoutMojo
+	@Test
+	public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
+	{
+	}
+}
